59ac4f3bb3db5d341b2e8fc3c9eef71a49f8c43a
Stephen Midgley <smidgley@pivotal.io>
2020-03-19 13:58:10 +0000

    Adding messages into automated testing

--- concourse/ci/autotests/test_1_create.py
+++ concourse/ci/autotests/test_1_create.py
@@ -1,32 +1,34 @@
 #file test_1_create.py
 from selenium import webdriver
-
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import time
 import os
 import datetime
+import urllib
+import sys
+
 zelEndpoint = os.environ['ZELENIUMENDPOINT']
 drupalEndpoint = os.environ['DRUPALENDPOINT']
 drupalUser = os.environ['DRUPALUSERNAME']
 drupalPass = os.environ['DRUPALPASSWORD']
 bowsertype = os.environ['BROWSERTYPE']
 
-testName = "Create Test using " + bowsertype + " T:" + datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+testName = "Create test using " + bowsertype + " T:" + datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S")
 
 driver = webdriver.Remote(
-   command_executor=zelEndpoint,
+   command_executor=zelEndpoint + "/wd/hub",
    desired_capabilities={
             "browserName": bowsertype,
             "platform": "LINUX",
             "name": testName
         })
-print ("Video: " + driver.session_id)
+
+
+print("Live view of test can be found here: " + zelEndpoint + "/dashboard/#")
 
 try:
     driver.implicitly_wait(30)
     #driver.maximize_window() # Note: driver.maximize_window does not work on Linux selenium version v2, instead set window size and window position like driver.set_window_position(0,0) and driver.set_window_size(1920,1080)
-
     driver.get(drupalEndpoint+"/user/login")
     driver.add_cookie({"name": "zaleniumMessage", "value": "Entering Admin Credentials"})
     elem = driver.find_element_by_id("edit-name")
@@ -50,13 +52,13 @@ try:
     elem = driver.find_element_by_id("edit-submit")
     elem.click()
     time.sleep(2)
-
-
-#except:
-#    driver.add_cookie({"name": "zaleniumTestPassed", "value": "false"})
-#    driver.quit()
-#    print("There was an error")
-#    sys.exit("There was an error, please check the logs")
+except Exception as E:
+   driver.add_cookie({"name": "zaleniumTestPassed", "value": "false"})
+   driver.quit()
+   print("There was an error" + E)
+   sys.exit("There was an error, please check the logs")
 finally:
+
+    print (zelEndpoint + "/dashboard/?q=" + urllib.parse.quote(testName))
     driver.add_cookie({"name": "zaleniumTestPassed", "value": "true"})
     driver.quit()
